f45a14307fbf55a43a81e9a962ab74720bd5f8b0
Saroj Dev <saroz@Sarojs-MacBook-Pro.local>
2022-06-03 16:55:01 -0500

    Add check out controller logic added.

--- src/main/java/com/mpp/librarysys/lms/services/BookService.java
+++ src/main/java/com/mpp/librarysys/lms/services/BookService.java
@@ -1,6 +1,7 @@
 package com.mpp.librarysys.lms.services;
 
 import com.mpp.librarysys.lms.entities.Book;
+import com.mpp.librarysys.lms.entities.BookCopy;
 import com.mpp.librarysys.lms.repository.BookRepository;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,6 +18,30 @@ public class BookService {
     public Book addNewBook(Book book) {
         bookRepository.save(book);
         return book;
+    }
+
+
+
+    public <T> ObservableList<T> getAllBooksByBookName(String bookName) {
+        ObservableList<Book> bookEntities = FXCollections.observableArrayList();
+        Book book = new Book();
+        book.setTitle("Java");
+        book.setId(1);
+        bookEntities.add(book);
+        return (ObservableList<T>) bookEntities;
+    }
+
+    public <T> ObservableList<T> getAllBookCopiesForSelectedBook(String bookName) {
+        ObservableList<BookCopy> bookEntities = FXCollections.observableArrayList();
+        BookCopy book = new BookCopy();
+        book.setCopyNumber(4);
+        book.setId(1);
+
+        BookCopy book1 = new BookCopy();
+        book.setCopyNumber(2);
+        book.setId(2);
+        bookEntities.add(book);
+        return (ObservableList<T>) bookEntities;
     }
 
     public <T> ObservableList<T> getBooksObs() {
--- src/main/java/com/mpp/librarysys/lms/services/CheckoutService.java
+++ src/main/java/com/mpp/librarysys/lms/services/CheckoutService.java
@@ -1,9 +1,19 @@
 package com.mpp.librarysys.lms.services;
 
+import com.mpp.librarysys.lms.entities.Book;
 import com.mpp.librarysys.lms.entities.CheckOutRecordBook;
+import com.mpp.librarysys.lms.entities.User;
+import com.mpp.librarysys.lms.entities.enums.RoleEnum;
+import com.mpp.librarysys.lms.repository.BookRepository;
 import com.mpp.librarysys.lms.repository.CheckOutRecordBookRepository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class CheckoutService {
@@ -11,8 +21,40 @@ public class CheckoutService {
     @Autowired
     private CheckOutRecordBookRepository checkOutRecordBookRepository;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     public CheckOutRecordBook addNewCheckOutRecordBook(CheckOutRecordBook checkOutRecordBook) {
         checkOutRecordBookRepository.save(checkOutRecordBook);
         return checkOutRecordBook;
     }
+
+    public List<Book> getAllBooks(){
+        List<Book> bookList = bookRepository.findAll();
+        return bookList;
+    }
+
+    public <T> ObservableList<T> getCheckOutObs() {
+        ObservableList<CheckOutRecordBook> userEntities = FXCollections.observableArrayList();
+
+        CheckOutRecordBook user = new CheckOutRecordBook();
+        user.setCheckOutDate(LocalDate.now());
+        user.setDueDate(LocalDate.now().plusDays(5));
+
+
+        CheckOutRecordBook user1 = new CheckOutRecordBook();
+        user.setCheckOutDate(LocalDate.now());
+        user.setDueDate(LocalDate.now().plusDays(7));
+
+        CheckOutRecordBook user2 = new CheckOutRecordBook();
+        user.setCheckOutDate(LocalDate.now());
+        user.setDueDate(LocalDate.now().plusDays(8));
+
+        userEntities.add(user);
+        userEntities.add(user1);
+        userEntities.add(user2);
+        return (ObservableList<T>) userEntities;
+    }
+
 }
+
